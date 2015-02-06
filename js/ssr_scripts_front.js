cfb47f3e8f41f61f45f01ed2a486d8f9694de0fb
saadamin <saadamin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 12:26:14 +0000

    fixed
    git-svn-id: https://plugins.svn.wordpress.org/simple-student-result/trunk@1083787 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/ssr_scripts_front.js
@@ -0,0 +1,5 @@
+/*
+Author: Saad Amin
+Website: http://www.saadamin.info
+*/
+jQuery(window).load(function(){jQuery("#rues").live("keyup",function(){jQuery("#rues").val().length>0?jQuery.post(SSR_Ajax.ajaxurl,{action:"ssr_view_st_submit",postID:jQuery.trim(jQuery("#rues").val())},function(e){if("no"!=jQuery.trim(e)){jQuery("#rid2").val(jQuery("#rues").val());var t=e.search("Rollg:XS")+8,n=e.search("Stdge:XS");jQuery("#rn2").val(e.substring(t,n));var t=e.search("Fxtge:XS");jQuery("#stn2").val(e.substring(n+8,t));var n=e.search("pYear:XS");jQuery("#stfn2").val(e.substring(t+8,n));var t=e.search("sCGPA:XS");jQuery("#stpy2").val(e.substring(n+8,t));var n=e.search("sSjct:XS");jQuery("#stcgpa2").val(e.substring(t+8,n));var t=e.search("stIme:XS");jQuery("#stsub2").val(e.substring(n+8,t));var n=e.length;jQuery("#st_img2").attr("src",e.substring(t+8,n)),jQuery(".result_box").css({opacity:1}),jQuery("#ssr_msgbox").css("display","none")}else jQuery(".result_box").css({opacity:0}),jQuery("#ssr_msgbox").css("display","block")}):(jQuery(".result_box").css({opacity:0}),jQuery("#ssr_msgbox").css("display","block"))});jQuery("#rues").keydown(function(e){return 32==e.keyCode?!1:void 0})})
