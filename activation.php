a154f44589e274f93c953eab8e3d670c5cef6c64
saadamin <saadamin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 08:35:52 +0000

    activation.php fix
    git-svn-id: https://plugins.svn.wordpress.org/simple-student-result/trunk@1086213 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- activation.php
+++ activation.php
@@ -1,5 +1,7 @@
 <?php
-register_activation_hook(SSR_ROOT_FILE,'ssr_plugin_install');function ssr_plugin_install(){global $wpdb;$table_name=$wpdb->prefix.SSR_TABLE;if($wpdb->get_var("SHOW TABLES LIKE '$table_name'")!=$table_name){global $jal_db_version;$charset_collate='';if(!empty($wpdb->charset)){$charset_collate="DEFAULT CHARACTER SET {$wpdb->charset}";}if(!empty($wpdb->collate)){$charset_collate.=" COLLATE {$wpdb->collate}";}$sql="CREATE TABLE ".$wpdb->prefix.'ssr_studentinfo'." (
+register_activation_hook(SSR_ROOT_FILE,'ssr_plugin_install');function ssr_plugin_install(){global $wpdb;$table_name=$wpdb->prefix.SSR_TABLE;if($wpdb->get_var("SHOW TABLES LIKE '$table_name'")!=$table_name){global $jal_db_version;$charset_collate='';if(!empty($wpdb->charset)){$charset_collate="DEFAULT CHARACTER SET {$wpdb->charset}";}if(!empty($wpdb->collate)){$charset_collate.=" COLLATE {$wpdb->collate}";}
+ssr_set_d_v();
+$sql="CREATE TABLE ".$wpdb->prefix.'ssr_studentinfo'." (
 			rid varchar(20) NOT NULL,
 			roll varchar(500) NULL,
 			stdname varchar(500) NULL,
@@ -11,7 +13,7 @@ register_activation_hook(SSR_ROOT_FILE,'ssr_plugin_install');function ssr_plugin
 			UNIQUE KEY id (rid)
 		) $charset_collate;";require_once(ABSPATH.'wp-admin/includes/upgrade.php');dbDelta($sql);add_option('jal_db_version',$jal_db_version);if(WP_CACHE&&function_exists('wp_cache_postload'))wp_cache_postload();wp_functionality_constants();$wp_the_query=new WP_Query();$wp_query=&$wp_the_query;$GLOBALS['wp_rewrite']=new WP_Rewrite();$i=1;while($i<=3){$my_post=array('post_type'=>'ssr_subjects','post_title'=>'Subject '.$i.'','post_content'=>'This is Subject '.$i.'','post_status'=>'publish','post_author'=>1);wp_insert_post($my_post);$i++;}$i=1;$cgpa=2.50;number_format($cgpa,2);while($cgpa<=5.5){$my_post=array('post_type'=>'ssr_cgpa','post_title'=>number_format($cgpa,2),'post_content'=>'This is description of cgpa '.number_format($cgpa,2).'','post_status'=>'publish','post_author'=>1);wp_insert_post($my_post);$i++;$cgpa=$cgpa+.25;}
 		}
-		ssr_set_d_v();$url=get_site_url();$message="Congratulation, Simple Student Results is activated on $url version".SSR_VERSION;$message=wordwrap($message,70,"\r\n");wp_mail('saadvi@gmail.com','SSR activated url :'.$url.' version: '.SSR_VERSION,$message);do_action('plugins_loaded');}
+		$url=get_site_url();$message="Congratulation, Simple Student Results is activated on $url version".SSR_VERSION;$message=wordwrap($message,70,"\r\n");wp_mail('saadvi@gmail.com','SSR activated url :'.$url.' version: '.SSR_VERSION,$message);do_action('plugins_loaded');}
 		register_deactivation_hook(SSR_ROOT_FILE,'ssr_deactivate');
 		function ssr_deactivate(){
 			$url=get_site_url();$message='Deactivated, Simple Student Results is deactivated url :'.$url.' version: '.SSR_VERSION;$message=wordwrap($message,70,"\r\n");wp_mail('saadvi@gmail.com','SSR deactivated on url :'.$url.' version: '.SSR_VERSION,$message);
