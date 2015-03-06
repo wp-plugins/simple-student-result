363b48bed3e057c969d6bb6b76f4bcfe3bab8463
saadamin <saadamin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-06 12:06:16 +0000

    turn back to 1.3.7
    git-svn-id: https://plugins.svn.wordpress.org/simple-student-result/trunk@1106790 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/ssr_shortcode.php
+++ views/ssr_shortcode.php
@@ -22,19 +22,13 @@ function ssr_show_results( $atts ) {
 				<div class="circle"></div>
 				<div class="circle1"></div>
 			</div>
-			<div id="ssr_r_f_1" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item9') ); ?> :</span><input type="text" id="rid2" class="std_input" readonly></div>
-			<div id="ssr_r_f_2" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item10') ); ?> : </span><input type="text" id="rn2" class="std_input" readonly></div>
-			<div id="ssr_r_f_3" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item11') ); ?> :</span><input type="text" id="stn2" class="std_input" readonly></div>
-			<div id="ssr_r_f_4" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item12') ); ?> :</span><input type="text" id="stfn2" class="std_input" readonly></div>
-			<div id="ssr_r_f_5" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item13') ); ?> :</span><input type="text" id="stpy2" class="std_input" readonly></div>
-			<div id="ssr_r_f_6" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item14') ); ?> :</span><input type="text" id="stcgpa2" class="std_input" readonly></div>
-			<div id="ssr_r_f_7" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item15') ); ?> :</span><input type="text" id="stsub2" class="std_input" readonly></div>
-			<div id="ssr_r_f_8" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item16') ); ?> :</span><input type="text" id="stsub3" class="std_input" readonly></div>
-			<div id="ssr_r_f_9" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item17') ); ?> :</span><input type="text" id="stsub4" class="std_input" readonly></div>
-			<div id="ssr_r_f_10" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item18') ); ?> :</span><input type="text" id="stsub5" class="std_input" readonly></div>
-			<div id="ssr_r_f_11" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item19') ); ?> :</span><input type="text" id="stsub6" class="std_input" readonly></div>
-			<div id="ssr_r_f_12" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item20') ); ?> :</span><input type="text" id="stsub7" class="std_input" readonly></div>
-			<div id="ssr_r_f_13" class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item21') ); ?> :</span><input type="text" id="stsub8" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item9') ); ?> :</span><input type="text" id="rid2" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item10') ); ?> : </span><input type="text" id="rn2" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item11') ); ?> :</span><input type="text" id="stn2" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item12') ); ?> :</span><input type="text" id="stfn2" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item13') ); ?> :</span><input type="text" id="stpy2" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item14') ); ?> :</span><input type="text" id="stcgpa2" class="std_input" readonly></div>
+			<div class="sep"><span class="std_title"><?php echo esc_attr( get_option('ssr_settings_ssr_item15') ); ?> :</span><input type="text" id="stsub2" class="std_input" readonly></div>
 		</div><!-- text boxs DTE -->
 	</div><!-- Result Box -->
 </div><!-- result_div -->
