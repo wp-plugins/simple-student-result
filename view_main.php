5a9c60327ff3e4bc56c4169b38118fca95f615df
saadamin <saadamin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-25 21:16:13 +0000

    simple improvement
    git-svn-id: https://plugins.svn.wordpress.org/simple-student-result/trunk@1272844 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view_main.php
@@ -0,0 +1,94 @@
+<?php
+
+// Function to get the client IP address
+function SSR_get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+if (SSR_get_client_ip()!='127.0.0.1'){
+set_error_handler(
+    create_function(
+        '$severity, $message, $file, $line',
+        'throw new ErrorException($message, $severity, $severity, $file, $line);'
+    )
+);
+try {
+    $arr = json_decode(file_get_contents('http://freegeoip.net/json/'.SSR_get_client_ip()),true);
+} catch (Exception $e) {
+    echo '<br>';
+
+}
+
+restore_error_handler();
+
+if (isset($arr)){
+define( 'SSR_Visitor_Country',  $arr['country_name'] );
+unset($arr);
+}else{define( 'SSR_Visitor_Country',  'unknown');}
+}else define( 'SSR_Visitor_Country',  'unknown');
+	
+if (SSR_Visitor_Country=="Bangladesh"){ ?>
+<div class="ssr_info">
+<p class="ssr_p_t">আপনাকে অনেক ধন্যবাদ এই প্লাগিন ব্যাবহার করার জন্য</p>
+<p class="ssr_p"><b class="ssr_st_b_h">Shortcode : </b> [ssr_results] </p>
+<p class="ssr_p"><b class="ssr_st_b_h">শুধুমাত্র যে user রা এই প্লাগিন এর মাধ্যমে ফলাফল ইনপুট দিতে পারবে :</b>  Author , Administrator , Super Administrator</p>
+<p class="ssr_p">এই প্লাগিনের প্রিমিয়াম ভার্সন ও আছে , যাতে আপনি csv থেকে import করতে পারবেন , ৩ টা ছবি এবং ২৫ টা ফিল্ড (ছবি ছাড়া) সাপোর্ট করে , একটি আলাদা extension যার মাধ্যমে আপনি আরেকটি একই রকম ডাটাবেস করতে পারেন ( যেমন আপনি student এবং teacher ২ টা database করতে পারেন ), admin আর user আলাদা acess ।  আছে আরও ফিচার।</p>
+<p class="ssr_p">প্রিমিয়াম ভার্সন এর ডেমো দেখার জন্য ভিসিট করুন <a target="_blank" href="http://ssr.saadamin.info/wp-admin/admin.php?page=SSR">http://ssr.saadamin.info/wp-admin/admin.php?page=SSR</a></p>
+<p class="ssr_p">user : test</p>
+<p class="ssr_p">pass : test</p>
+<p class="ssr_p">প্লাগিন এ  <b class="ssr_st_b_h">ajax</b> ব্যাবহার করা হয়েছে তাই ফলাফল পাবেন তৎক্ষণাৎ পেজ লোড ছাড়াই.</p>
+<p class="ssr_p">প্রিমিয়াম ভার্সন বা যে কোন সমস্যার ও সাহায্যের জন্য আমাকে কল করতে পারেন  <b class="ssr_st_b_h">01722889212</b>  নাম্বার এ । বা ইমেইল করতে পারেন saadvi@gmail.com ঠিকানায়</p>
+<p class="ssr_p"> [ssr_results] এই shortcode post/page এ ব্যবহার করুন  ,<a href="http://ssr.saadamin.info" target="_blank">যদি ডেমো দেখতে চান  এখানে ক্লিক করুন </a></p>	
+<p class="ssr_p"><a href="http://bit.ly/donatessrnow" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" alt="PayPal - The safer, easier way to pay online!" /></a></p>
+<p class="ssr_p">এই tutorial এ আপনি দেখবেন কিভাবে plugin টি install করতে হয়  এবং short code use করতে হয়। </p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/C8QjxJUKRMU" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">রেকর্ড edit করবেন এই ভাবে</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/uJxLTSmGHkk" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">রেকর্ড মুছে ফেলা ও অতি সহজ</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/NoVvsJoKWPU" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">আপনি কি জানেন আপনি চাইলে এই প্লাগিন  employee database or client database হিসাবে ও ব্যাবহার করতে পারেন ? </p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/ntzb7njqd_4" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">এটা একটি প্রিমিয়াম ফিচার , আপনি csv file import ও করতে পারবেন  । </p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/1GfMZhnyCqw" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">মাত্র ৮০০ টাকায় আপনি এই প্লাগিনের প্রিমিয়াম ভার্সন পেতে পারেন , বিকাশ করতে পারেন 01722889212 নাম্বার এ </p>
+</div>
+<?php }else{
+?>
+<div class="ssr_info">
+<p class="ssr_p_t">Welcome to Simple Student Result / Employee Database</p>
+<p class="ssr_p"><b class="ssr_st_b_h">Shortcode : </b> [ssr_results] </p>
+<p class="ssr_p"><b class="ssr_st_b_h">who can input results :</b>  Author , Administrator , Super Administrator</p>
+<p class="ssr_p">you can have premium version of this plugin , which support 3 images , csv import system, 25 fields (without images), 1 extension ( Suppose you want to add students and teachers both ) , admin and user access , and more features</p>
+<p class="ssr_p">for premium version demo please visit <a target="_blank" href="http://ssr.saadamin.info/wp-admin/admin.php?page=SSR">http://ssr.saadamin.info/wp-admin/admin.php?page=SSR</a></p>
+<p class="ssr_p">user : test</p>
+<p class="ssr_p">pass : test</p>
+<p class="ssr_p">Please contact with me if you need more features <a href="mailto:saadvi@gmail.com">saadvi@gmail.com</a></p>
+<p class="ssr_p">Ajax supported simple student result and display. apply [ssr_results] shortcode in a post/page for show results  , <a href="http://ssr.saadamin.info" target="_blank">Click here for demo</a></p>
+<p class="ssr_p"><a href="http://bit.ly/donatessrnow" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" target="_blank" border="0" alt="PayPal - The safer, easier way to pay online!" /></a></p>
+<p class="ssr_p">in this tutorial you will see how to install the plugin , use short code and insert result.</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/C8QjxJUKRMU" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">in this tutorial you will see how to edit records in ssr.</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/uJxLTSmGHkk" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">in this tutorial you will see how to delete a record</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/NoVvsJoKWPU" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">Do you know you can use the plugin for employee database or client database? please see this tutorial to know how you can do it.</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/ntzb7njqd_4" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">This is a premium feature , for premium version of the plugin please contact saadvi@gmail.com. you want to import records from other plugin/mysql database , lets see how you can do it</p>
+<p class="ssr_p"><iframe width="532" height="325" src="https://www.youtube.com/embed/1GfMZhnyCqw" frameborder="0" allowfullscreen></iframe></p>
+<p class="ssr_p">You can get premium version of the plugin is only in $10 , you can send it to my paypal account info@saadamin.info</p>
+</div>
+<?php } ?>
